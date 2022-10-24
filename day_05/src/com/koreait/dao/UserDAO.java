2b398d6a23d03ab928e9d1b91bc481eb43a8a8d7
YuYoHan <zxzz8014@naver.com>
2022-10-24 18:33:23 +0900

    [Study] dbcp

--- day_05/src/com/koreait/dao/UserDAO.java
+++ day_05/src/com/koreait/dao/UserDAO.java
@@ -2,19 +2,53 @@ package com.koreait.dao;
 
 import com.koreait.dto.UserDTO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
 
 public class UserDAO {
+    Context context;
+    DataSource ds;
+
     Connection conn;
     PreparedStatement ps;
     ResultSet rs;
 
     public boolean join(UserDTO newUser) {
+        int result = 0;
 
+        try {
+            context = new InitialContext(null);
+            ds =(DataSource)context.lookup("java:comp/env/jdbc/mysql");
+            conn = ds.getConnection();
 
+            String sql = "insert into test_user values(?,?,?,?,?,?,?,?,?)";
 
-        return false;
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, newUser.getUserid());
+            ps.setString(2, newUser.getUserpw());
+            ps.setString(3, newUser.getUsername());
+            ps.setString(4, newUser.getUsergender());
+            ps.setString(5, newUser.getZipcode());
+            ps.setString(6, newUser.getAddr());
+            ps.setString(7, newUser.getAddrdetail());
+            ps.setString(8, newUser.getAddretc());
+
+            String[] hobbies = newUser.getUserhobby();  // [게임, 운동, 코딩]
+            String hobbyStr = hobbies[0];               // "게임, 운동, 코딩"
+            for (int i =1; i < hobbies.length; i++) {
+                hobbyStr += ", " + hobbies[i];
+            }
+            ps.setString(9, hobbyStr);
+            result = ps.executeUpdate();
+
+        } catch (NamingException e) {
+            System.out.println("네이밍 오류 : " + e);
+        } catch (SQLException se) {
+            System.out.println("SQL오류 : " + se);
+        }
+        return result == 1;
     }
 }
--- day_05/web/join_db.jsp
+++ day_05/web/join_db.jsp
@@ -1,3 +1,5 @@
+<%@ page import="com.koreait.dto.UserDTO" %>
+<%@ page import="com.koreait.dao.UserDAO" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <% request.setCharacterEncoding("UTF-8"); %>
 <jsp:useBean id="newUser" class="com.koreait.dto.UserDTO"></jsp:useBean>
@@ -9,6 +11,20 @@
 //    for(int i=0; i < hobbies.length; i++) {
 //        System.out.println(hobbies[i]);
 //    }
-
-
+    UserDAO udao = new UserDAO();
+    if(udao.join(newUser)) {
+%>
+    <script>
+        alert("회원가입을 성공했습니다");
+        location.href = "loginview.jsp"
+    </script>
+<%
+    } else {
+ %>
+    <script>
+        alert("잠시후에 시도하세요");
+        location.href = "joinView.jsp"
+    </script>
+<%
+}
 %>
